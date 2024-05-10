6fd07feea1847a922708a7481374da07a341dd55
Anita <anitastrawberrymiao@gmail.com>
2024-05-10 22:06:24 +0200

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ThankyouPageComponent } from './pages/thankyou-page/thankyou-page.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full', }
+    { path: 'home', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'done', component: ThankyouPageComponent },
+    { path: '**', component: ErrorPageComponent }
+];
---
+++ src/app/pages/thankyou-page/thankyou-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thankyou-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './thankyou-page.component.html',
+  styleUrl: './thankyou-page.component.scss'
+})
+export class ThankyouPageComponent {
+
+}
